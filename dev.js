dace24f6b475b8dc507c6674770a94dcbe93a842
Pablo Porto <portovep@tcd.ie>
2016-03-17 07:37:56 +0000

    Migrate to Restify from Express.js
    Restify provides an easier way to create REST web services.
    Added npm start script.

--- dev.js
+++ dev.js
@@ -1,10 +1,13 @@
 // Dev server to run the app
 
-var express = require('express');
-var app = express();
+var restify = require('restify');
+var server = restify.createServer();
 
-app.use('/', express.static(__dirname + '/public'))
+server.get(/\//, restify.serveStatic({
+  directory: './public',
+  default: 'index.html'
+}));
 
-app.listen(3000, function () {
-  console.log('Dev server up and rocking on port 3000!');
+server.listen(3000, function() {
+  console.log('Dev server up and rocking on %s', server.url);
 });
