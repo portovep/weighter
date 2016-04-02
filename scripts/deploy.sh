3339952ca1bbd01bd2a3cefa9a80f228c4d5a715
Pablo Porto <portovep@tcd.ie>
2016-04-02 02:51:34 +0100

    Fix typo in deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ ENVIRONMENT=${1:-staging}
 git remote add target-remote https://git.heroku.com/weighter-${ENVIRONMENT}.git
 
 # tell Heroku to install additional dev dependencies to be able to build the app during deployment
-heroku config:set NPM_CONFIG_PRODUCTION=false --remote staging
+heroku config:set NPM_CONFIG_PRODUCTION=false --remote target-remote
 
 # deploy app
 git push target-remote master
