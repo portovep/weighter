49ee94594c1e44273ff6ee4f1e84e6b8782631df
Pablo Porto <portovep@tcd.ie>
2016-04-02 02:46:44 +0100

    Fix deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,9 @@ ENVIRONMENT=${1:-staging}
 # create target remote
 git remote add target-remote https://git.heroku.com/weighter-${ENVIRONMENT}.git
 
+# tell Heroku to install additional dev dependencies to be able to build the app during deployment
+heroku config:set NPM_CONFIG_PRODUCTION=false --remote staging
+
 # deploy app
 git push target-remote master
 
