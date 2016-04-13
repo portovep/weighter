d1429122f7e1fab5c6c64f224218fb218e781942
Pablo Porto <porto.vga@gmail.com>
2016-04-13 19:24:17 +0100

    Log error when database URL env var is not set

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,12 +1,17 @@
 
-module.exports = {
-  client: 'postgresql',
-  connection: process.env.DATABASE_URL,
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
-};
+if (!process.env.DATABASE_URL) {
+  console.error('ERROR: The environment variable DATABASE_URL is not set');
+}
+else {
+  module.exports = {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  };
+}
